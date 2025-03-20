c731cfb261fc01ebb2ea1c8f7a785d908a960966
baohuy.dev <lenambaohuy107@gmail.com>
2025-03-20 17:01:10 +0700

    init

---
+++ src/app/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="w-full">
+            <nav>
+                {/* Add your navigation content here */}
+                <h1 className='text-mini'>
+                    huy đẹp trai
+                </h1>
+            </nav>
+        </header>
+    );
+};
+export default Header;
